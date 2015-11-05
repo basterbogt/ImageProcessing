b2aa0f3e830e222ac2d3f82acc08617be82ce2bc
basterbogt <bas@basterbogt.com>
2015-11-05 21:06:14 +0100

    Small bug fix - Addition & Subtraction

--- Operations/Subtraction.cs
+++ Operations/Subtraction.cs
@@ -10,13 +10,14 @@
         public static Image Apply(Image Image, Image Image2)
         {
             Image newImg = new Image(Image.GetPixels(), Image.Size);
-            int additive_value = Image.Black;
             for (int x = 0; x < Image.Size.Width; x++)
             {
                 for (int y = 0; y < Image.Size.Height; y++)
                 {
-                    additive_value = (Image.GetPixelColor(x, y)- Image2.GetPixelColor(x, y));
-                    if (additive_value < 0) additive_value = Image.Black;
+                    int additive_value = (Image.GetPixelColor(x, y)- Image2.GetPixelColor(x, y));
+                    //if (additive_value < 0) additive_value = Image.Black;
+                    int highestValue = Image.White;
+                    additive_value = (int)((additive_value + highestValue) / 2.0f);//This line will re-balance the values from [ -225, 255 ], to [ 0 , 255 ]
                     newImg.SetPixelColor(x, y, additive_value);
                 }
             }
